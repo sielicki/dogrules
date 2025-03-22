0acea18280f9a1c2c9553599c597dc2332adfef4
Nicholas Sielicki <nix@opensource.nslick.com>
2025-03-22 10:30:30 -0700

    fix: cleanup commented out shit
    Signed-off-by: Nicholas Sielicki <nix@opensource.nslick.com>

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   inputs.flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
   inputs.nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
   inputs.systems.url = "github:nix-systems/default";
-  #inputs.flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/0.1.369.tar.gz";
-  #inputs.nix-unit.url = "github:nix-community/nix-unit";
+  inputs.flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/0.1.369.tar.gz";
+  inputs.nix.url = "github:DeterminateSystems/nix-src/flake-schemas";
 
-  outputs = {self, ... }@inputs:
-    #inputs.flake-parts.lib.mkFlake { inherit inputs; } ({...}:
+  outputs = {...}@inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } ({...}:
     let
       lib = inputs.nixpkgs-lib.lib;
       inherit (builtins)
@@ -39,32 +39,14 @@
       ]
       mkPlatformSchema;
 
-      #mkDummyFlake = testCaseName: {
-      #    inputs = {
-      #      dogrules.url = "${builtins.storePath self}";
-      #      flake-schemas.url = "${builtins.storePath inputs.flake-schemas}";
-      #    };
-      #    outputs = inputs.flake-parts.lib.mkFlake { inputs = {}; }({...}:{ systems = [ "aarch64-darwin" ]; flake.foo = "bar"; });
-      #};
-
     in
     {
-      #systems = import inputs.systems;
-      #imports = [
-      #  inputs.nix-unit.modules.flake.default
-      #];
-      #perSystem = {config,pkgs,...}: {
-      #  #packages.testFiles =
-      #};
-      #flake = {
-      #  inherit mkDummyFlake;
-        schemas = lib.mergeAttrs inputs.flake-schemas.schemas localSchema;
-      #  tests.testBar = {
-      #    expr = "bar";
-      #    expected = "bar";
-      #  };
-      #};
-    }
-    #)
-    ;
+      systems = import inputs.systems;
+      perSystem = {inputs',...}: {
+        packages.nix = inputs'.nix.packages.default;
+      };
+      flake = {
+        schemas = localSchema;
+      };
+    });
 }
